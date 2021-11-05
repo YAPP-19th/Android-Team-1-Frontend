d69a35a7f4cb69116dbf2c862d5c20026fc05136
plat67 <jihee.dev@gmail.com>
2021-11-05 20:17:09 +0900

    feat(splash): Implement Splash

--- DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/MainActivity.kt
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/MainActivity.kt
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+
+        }
     }
 
 }
