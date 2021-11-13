fd5612b0e89ee3ef72e56ffbe37800352ca51186
Jihee.Han <jihee.dev@gmail.com>
2021-11-14 04:45:24 +0900

    feat(permission): use custom permission dialog

--- DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/permission/PermissionActivity.kt
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/permission/PermissionActivity.kt
@@ -2,36 +2,60 @@ package yapp.android1.delibuddy.ui.permission
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import dagger.hilt.android.AndroidEntryPoint
 import yapp.android1.delibuddy.databinding.ActivityPermissionBinding
 import yapp.android1.delibuddy.ui.MainActivity
 import yapp.android1.delibuddy.util.intentTo
 import yapp.android1.delibuddy.util.permission.CheckPermission
+import yapp.android1.delibuddy.util.permission.PermissionType
+import yapp.android1.delibuddy.util.permission.getPermissionsFlag
+import java.util.jar.Manifest
 
 @AndroidEntryPoint
 class PermissionActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPermissionBinding
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
+    private var requestedPermissions = emptyList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPermissionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initPermissionLauncher()
         init()
+    }
+
+    private fun initPermissionLauncher() {
+        requestedPermissions = getPermissionsFlag(
+            arrayOf(PermissionType.LOCATION)
+        )
+
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when (permissions.entries.filter { it.value }.size == requestedPermissions.size) {
+                true -> permissionGranted()
+                false -> permissionDenied()
+            }
+        }
     }
 
     private fun init() = with(binding) {
         btnPermission.setOnClickListener {
             CheckPermission.checkPermission(
                 this@PermissionActivity,
+                requestPermissionLauncher,
                 {
-                    when(it) {
+                    when (it) {
                         true -> permissionGranted()
                         false -> permissionDenied()
                     }
-                }
-
+                },
+                requestedPermissions
             )
         }
     }
--- DeliBuddy/app/src/main/java/yapp/android1/delibuddy/util/permission/CheckPermission.kt
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/util/permission/CheckPermission.kt
@@ -1,37 +1,44 @@
 package yapp.android1.delibuddy.util.permission
 
 import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
+import yapp.android1.delibuddy.ui.dialog.PermissionDialogFragment
 
 object CheckPermission {
     fun checkPermission(
         context: AppCompatActivity,
+        requestPermissionLauncher: ActivityResultLauncher<Array<String>>,
         permissionCallback: (isGranted: Boolean) -> Unit,
-        vararg type: PermissionType
+        //vararg type: PermissionType
+        requestedPermissions: List<String>
     ) {
-        val requestedPermissions: List<String> = getPermissionsFlag(type)
+        //val requestedPermissions: List<String> = getPermissionsFlag(type)
 
         if (requestedPermissions.isEmpty())
             return
 
-        val requestPermissionLauncher = context.registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permissions ->
-            val isGranted =
-                permissions.entries.filter { it.value }.size == requestedPermissions.size
-            permissionCallback(isGranted)
-        }
+//        val requestPermissionLauncher = context.registerForActivityResult(
+//            ActivityResultContracts.RequestMultiplePermissions()
+//        ) { permissions ->
+//            val isGranted =
+//                permissions.entries.filter { it.value }.size == requestedPermissions.size
+//            permissionCallback(isGranted)
+//        }
 
         when {
             checkPermissionIsGranted(context, requestedPermissions) ->
                 permissionCallback(true)
             checkAnyPermissionIsDenied(context, requestedPermissions) -> {
-                
+                requestPermissionLauncher.launch(requestedPermissions.toTypedArray())
             }
-            else -> requestPermissionLauncher.launch(requestedPermissions.toTypedArray())
+            else -> {
+                val permissionDialog = PermissionDialogFragment(context)
+                permissionDialog.show(context.supportFragmentManager, null)
+            }
         }
     }
 
