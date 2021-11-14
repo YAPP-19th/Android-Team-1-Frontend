db83367e508a4427e66458da2fdaa73d5bb345ad
김효린 <hyorin97@naver.com>
2021-11-14 22:51:14 +0900

    feat(navigation): 하단 네비게이션 fragment 연결
    - fragment 생성
    - fragment 연결
    - 뒤로가기 버튼 고려

---
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/home/fragments/HomeFragment.kt
@@ -0,0 +1,8 @@
+package yapp.android1.delibuddy.ui.home.fragments
+
+import yapp.android1.delibuddy.base.BaseFragment
+import yapp.android1.delibuddy.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(
+    FragmentHomeBinding::inflate
+)
