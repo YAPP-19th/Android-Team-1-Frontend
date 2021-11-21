94c099d8921b13c88c18aa1ee728b548a9815452
Joseph <wjsdudtkd95@gmail.com>
2021-11-21 19:28:44 +0900

    feat(partyInfo): partyinformation XML 작업 진행중 (40%..)
    - 댓글 입력창 진행중

--- DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/partyInformation/PartyInformationActivity.kt
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/partyInformation/PartyInformationActivity.kt
@@ -1,10 +1,16 @@
-package yapp.android1.delibuddy.ui.partyDetail
+package yapp.android1.delibuddy.ui.partyInformation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import yapp.android1.delibuddy.R
+import yapp.android1.delibuddy.databinding.ActivityPartyInformationBinding
 
+@AndroidEntryPoint
 class PartyInformationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPartyInformationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_party_information)
