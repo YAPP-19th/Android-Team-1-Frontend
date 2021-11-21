9a94d26d57e60b46c28b82daf1314d72c871a5ad
Joseph <wjsdudtkd95@gmail.com>
2021-11-21 15:47:04 +0900

    refactor(partyInfo): FoodType Sealedclass로 생성

--- DeliBuddy/app/src/main/java/yapp/android1/delibuddy/model/FoodType.kt
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/model/FoodType.kt
@@ -1,6 +1,41 @@
 package yapp.android1.delibuddy.model
 
+import yapp.android1.delibuddy.R
 
-internal enum class FoodType {
-    BREAD, BURGER, CAFE, CHICKEN, CHINESE, JAPANESE, KOREAN, PIZZA, SEAFOOD, SNACKBAR, WESTERN
+internal sealed class FoodType(
+    val type: String,
+    val colorResource: Int,
+    val bigIconResource: Int,
+) {
+    object Bread : FoodType(type = "Bread", colorResource = R.color.bread_background, bigIconResource = R.drawable.big_food_icon_bread)
+    object Burger : FoodType(type = "Burger", colorResource = R.color.burger_background, bigIconResource = R.drawable.big_food_icon_burger)
+    object Cafe : FoodType(type = "Cafe", colorResource = R.color.cafe_background, bigIconResource = R.drawable.big_food_icon_cafe)
+    object Chicken : FoodType(type = "Chicken", colorResource = R.color.chicken_background, bigIconResource = R.drawable.big_food_icon_chicken)
+    object Chinese : FoodType(type = "Chinese", colorResource = R.color.chinese_background, bigIconResource = R.drawable.big_food_icon_chinese)
+    object Japanese : FoodType(type = "Japanese", colorResource = R.color.japanese_background, bigIconResource = R.drawable.big_food_icon_japanese)
+    object Korean : FoodType(type = "Korean", colorResource = R.color.korean_background, bigIconResource = R.drawable.big_food_icon_korean)
+    object Pizza : FoodType(type = "Pizza", colorResource = R.color.pizza_background, bigIconResource = R.drawable.big_food_icon_pizza)
+    object Seafood : FoodType(type = "Seafood", colorResource = R.color.seafood_background, bigIconResource = R.drawable.big_food_icon_seafood)
+    object Snackbar : FoodType(type = "Snackbar", colorResource = R.color.snackbar_background, bigIconResource = R.drawable.big_food_icon_snackbar)
+    object Western : FoodType(type = "Western", colorResource = R.color.western_background, bigIconResource = R.drawable.big_food_icon_western)
+
+    companion object {
+        fun of(type: String): FoodType {
+            return when (type) {
+                "Bread"     -> Bread
+                "Burger"    -> Burger
+                "Cafe"      -> Cafe
+                "Chicken"   -> Chicken
+                "Chinese"   -> Chinese
+                "Japanese"  -> Japanese
+                "Korean"    -> Korean
+                "Pizza"     -> Pizza
+                "Seafood"   -> Seafood
+                "Snackbar"  -> Snackbar
+                "Western"   -> Western
+                else -> throw IllegalArgumentException("올바른 타입이 아닙니다")
+            }
+        }
+    }
 }
+
