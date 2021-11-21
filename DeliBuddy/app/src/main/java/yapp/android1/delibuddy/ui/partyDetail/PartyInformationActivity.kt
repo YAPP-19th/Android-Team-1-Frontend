1dcb31936736a778b8cebefde2dbab93da401e57
Joseph <wjsdudtkd95@gmail.com>
2021-11-21 15:49:43 +0900

    fix(partyInfo): Activity이름 변경

--- DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/partyDetail/PartyInformationActivity.kt
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/partyDetail/PartyInformationActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import yapp.android1.delibuddy.R
 
-class PartyDetailActivity : AppCompatActivity() {
+class PartyInformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_party_detail)
+        setContentView(R.layout.activity_party_information)
     }
 }
