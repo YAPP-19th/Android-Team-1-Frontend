afe8a2a49166caf3b2732e8c354d0a10defea843
Joseph <wjsdudtkd95@gmail.com>
2021-12-04 21:17:14 +0900

    feat(partyInfo): CommentAdapter 답글쓰기 리스너 등록 및 구조 개선
    - ViewHolder를 다른 파일로 분리
    - 가독성 개선

--- DeliBuddy/app/src/main/java/yapp/android1/delibuddy/adapter/CommentAdapter.kt
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/adapter/CommentAdapter.kt
@@ -4,22 +4,29 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
-import com.bumptech.glide.Glide
-import yapp.android1.delibuddy.databinding.ItemChildCommentBinding
-import yapp.android1.delibuddy.databinding.ItemParentCommentBinding
 import yapp.android1.delibuddy.model.Comment
 
+typealias WriteReplyListener = (Comment) -> Unit
 
 class CommentAdapter : ListAdapter<Comment, CommentViewHolder>(CommentDiffUtil()) {
+
+    private var writeReplyListener: WriteReplyListener? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return CommentViewHolder.of(layoutInflater, viewType)
     }
 
     override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
-        holder.onBind(currentList[position])
+        holder.onBind(currentList[position], writeReplyListener!!)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return currentList[position].viewType
+    }
+
+    fun setWriteReplyListener(listener: WriteReplyListener) {
+        writeReplyListener = listener
     }
 
 }
@@ -33,49 +40,4 @@ class CommentDiffUtil() : DiffUtil.ItemCallback<Comment>() {
         return oldItem.id == newItem.id
     }
 
-}
-
-class CommentViewHolder private constructor(
-    private val binding: ViewBinding
-) : RecyclerView.ViewHolder(binding.root) {
-
-    fun onBind(comment: Comment) {
-        when(binding) {
-            is ItemParentCommentBinding -> {
-                bindParentComment(binding, comment)
-            }
-
-            is ItemChildCommentBinding -> {
-                bindChildComment(binding, comment)
-            }
-        }
-    }
-
-    private fun bindParentComment(binding: ItemParentCommentBinding, comment: Comment) = with(binding) {
-        tvWriterNickname.text = comment.writer.nickName
-        binding.tvWriteComment.setOnClickListener {
-
-        }
-        Glide.with(itemView.context)
-            .load(comment.writer.profileImage)
-            .into(binding.ivUserIcon)
-    }
-
-    private fun bindChildComment(binding: ItemChildCommentBinding, comment: Comment) = with(binding) {
-
-    }
-
-    companion object {
-        fun of(layoutInflater: LayoutInflater, viewType: Int): CommentViewHolder {
-            return when (viewType) {
-                Comment.PARENT -> {
-                    CommentViewHolder(ItemParentCommentBinding.inflate(layoutInflater))
-                }
-                Comment.CHILD -> {
-                    CommentViewHolder(ItemChildCommentBinding.inflate(layoutInflater))
-                }
-                else -> throw IllegalArgumentException("올바른 Comment 타입이 아닙니다.")
-            }
-        }
-    }
 }
---
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/adapter/CommentViewHolder.kt
@@ -0,0 +1,72 @@
+package yapp.android1.delibuddy.adapter
+
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.bumptech.glide.Glide
+import yapp.android1.delibuddy.databinding.ItemChildCommentBinding
+import yapp.android1.delibuddy.databinding.ItemParentCommentBinding
+import yapp.android1.delibuddy.model.Comment
+
+
+class CommentViewHolder private constructor(
+    private val binding: ViewBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun onBind(comment: Comment, listener: WriteReplyListener) {
+        when (binding) {
+            is ItemParentCommentBinding -> {
+                bindParentComment(binding, comment, listener)
+            }
+
+            is ItemChildCommentBinding -> {
+                bindChildComment(binding, comment)
+            }
+        }
+    }
+
+    private fun bindParentComment(
+        binding: ItemParentCommentBinding,
+        comment: Comment,
+        listener: WriteReplyListener
+
+    ) = with(binding) {
+        tvWriterNickname.text = comment.writer.nickName
+        tvBody.text = comment.body
+
+        tvWriteComment.setOnClickListener {
+            listener.invoke(comment)
+        }
+
+        Glide.with(itemView.context)
+            .load(comment.writer.profileImage)
+            .into(binding.ivUserIcon)
+    }
+
+    private fun bindChildComment(
+        binding: ItemChildCommentBinding,
+        comment: Comment
+
+    ) = with(binding) {
+        tvWriterNickname.text = comment.writer.nickName
+        tvBody.text = comment.body
+
+        Glide.with(itemView.context)
+            .load(comment.writer.profileImage)
+            .into(binding.ivUserIcon)
+    }
+
+    companion object {
+        fun of(layoutInflater: LayoutInflater, viewType: Int): CommentViewHolder {
+            return when (viewType) {
+                Comment.PARENT -> {
+                    CommentViewHolder(ItemParentCommentBinding.inflate(layoutInflater))
+                }
+                Comment.CHILD -> {
+                    CommentViewHolder(ItemChildCommentBinding.inflate(layoutInflater))
+                }
+                else -> throw IllegalArgumentException("올바른 Comment 타입이 아닙니다.")
+            }
+        }
+    }
+}
