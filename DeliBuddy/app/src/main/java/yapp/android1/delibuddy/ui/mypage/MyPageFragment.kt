2d78eb5b2eda9d7c577e2e17e1545d843cb72cbc
jsh-me <ppm_it@naver.com>
2022-01-05 01:50:05 +0900

    implement mypage

--- DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/mypage/MyPageFragment.kt
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/mypage/MyPageFragment.kt
@@ -1,8 +1,61 @@
 package yapp.android1.delibuddy.ui.mypage
 
+import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.view.View
+import androidx.activity.viewModels
+import androidx.core.text.bold
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import yapp.android1.delibuddy.R
 import yapp.android1.delibuddy.base.BaseFragment
 import yapp.android1.delibuddy.databinding.FragmentMypageBinding
+import yapp.android1.delibuddy.ui.myparty.adapter.MyPartyMockGenerator
+import yapp.android1.delibuddy.ui.partyInformation.PartyInformationViewModel
+import yapp.android1.delibuddy.util.extensions.repeatOnStarted
+import yapp.android1.delibuddy.util.intentTo
 
+@AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMypageBinding>(
     FragmentMypageBinding::inflate
-)
+) {
+    private val viewModel by viewModels<MyPageViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        collectData()
+    }
+
+    private fun initViews() {
+        binding.tvLicense.setOnClickListener {
+            requireContext().intentTo(OssLicensesMenuActivity::class.java)
+        }
+    }
+
+    private fun collectData() {
+        repeatOnStarted {
+            viewModel.myInfo.collect {
+                binding.tvUsername.text = it?.nickname
+                binding.tvBannerTitle.text =
+                    SpannableStringBuilder()
+                        .append(getString(R.string.navigation_mypage_banner_title))
+                        .append(" ")
+                        .bold {
+                            append(getString(R.string.navigation_mypage_count, it?.partiesCnt ?: 0))
+                        }
+
+                Glide.with(binding.ivProfile)
+                    .load(it?.profileImage)
+                    .into(binding.ivProfile)
+            }
+
+            viewModel.currentAddress.collect {
+                binding.tvLocation.text = it?.addressName
+            }
+        }
+    }
+}
---
+++ DeliBuddy/app/src/main/java/yapp/android1/delibuddy/ui/mypage/MyPageViewModel.kt
@@ -0,0 +1,60 @@
+package yapp.android1.delibuddy.ui.mypage
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import yapp.android1.delibuddy.DeliBuddyApplication
+import yapp.android1.delibuddy.base.BaseViewModel
+import yapp.android1.delibuddy.base.RetryAction
+import yapp.android1.delibuddy.model.Address
+import yapp.android1.delibuddy.model.Category
+import yapp.android1.delibuddy.model.Event
+import yapp.android1.domain.NetworkResult
+import yapp.android1.domain.entity.UserEntity
+import yapp.android1.domain.interactor.usecase.GetMyInfoUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageViewModel @Inject constructor(
+    private val getMyInfoUseCase: GetMyInfoUseCase,
+) : BaseViewModel<MyPageViewModel.MyPageEvent>() {
+
+    private val _myInfo = MutableStateFlow<UserEntity?>(null)
+    val myInfo: MutableStateFlow<UserEntity?> = _myInfo
+
+    private val _currentAddress = MutableStateFlow<Address?>(null)
+    val currentAddress: StateFlow<Address?> = _currentAddress
+
+    sealed class MyPageEvent : Event {
+
+    }
+
+    init {
+        fetchData()
+    }
+
+    private fun fetchData() {
+        viewModelScope.launch {
+            when (val result = getMyInfoUseCase.invoke()) {
+                is NetworkResult.Success -> {
+                    _myInfo.value = result.data
+                    _currentAddress.value = DeliBuddyApplication.prefs.getCurrentUserAddress()
+                }
+
+                is NetworkResult.Error -> handleError(result) {
+
+                }
+            }
+        }
+    }
+
+    override suspend fun handleEvent(event: MyPageEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun handleError(result: NetworkResult.Error, retryAction: RetryAction?) {}
+}
